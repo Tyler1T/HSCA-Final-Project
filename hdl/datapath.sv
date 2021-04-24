1587663b5286b14a524137cbcdc58d6f2bd0b1b0
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-04-24 17:30:21 -0500

    Added register to datapath

--- hdl/datapath.sv
+++ hdl/datapath.sv
@@ -1,3 +1,13 @@
-module datapath();
+module datapath(input   logic        clk, reset,
+                input   logic        N, D, IA,
+                output  logic [31:0] result);
 
+    wire CSAM_register;
+
+    CSAM multiplier(.Z(CSAM_register));
+    flopr  #(32) output(.clk(clk),
+                        .reset(reset),
+                        .en(MemSysReady),
+                        .d(CSAM_register),
+                        .q(result));
 endmodule
