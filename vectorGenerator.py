96e3a8b9c9d2edcac4277c04870f52b5f19ad40d
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-04-26 09:57:20 -0500

    Created a python program to make pseudo random test vectors and write them to the test vector file

---
+++ vectorGenerator.py
@@ -0,0 +1,28 @@
+import random
+
+# Python program to convert decimal to binary
+
+# Function to convert Decimal number
+# to Binary number
+
+def decimalToNBinary(q, n):
+    return format(q, "0" + str(n) + "b")
+
+# Driver code
+if __name__ == '__main__':
+    f = open("hdl/testvector.tv", "w+")
+    x = 0
+    y = 0
+    z = 0
+    for i in range(0, 30):
+        x = random.randint(1, 65535)
+        y = random.randint(1, 65535)
+        z = x*y
+        f.write(decimalToNBinary(z, 32))
+        f.write("_")
+        f.write(decimalToNBinary(x, 16))
+        f.write("_")
+        f.write(decimalToNBinary(y, 16))
+        f.write("\n")
+        print()
+    f.close()
