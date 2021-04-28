02543addfbd4b5b314c964237f4571c158f63786
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-04-28 15:46:54 -0500

    RNE should be 19 bits

--- hdl/RNE.sv
+++ hdl/RNE.sv
@@ -2,6 +2,6 @@ module RNE(input logic[37:0] big,
             output logic [18:0] rounded);
 
     assign rounded[18:1] = big[36:18];
-    assign rounded[0] = big[21] & big[20] | ~big[21] & big[20] & big[19];
+    assign rounded[0] = big[18] & big[17] | ~big[18] & big[17] & big[16];
 
 endmodule
