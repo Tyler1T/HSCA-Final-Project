a435bd588de9617a1306a908d0218e3ae992cf9f
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-04-28 21:47:01 -0500

    Inputs and outputs were in the wrong order for AND gates

--- hdl/mux4.sv
+++ hdl/mux4.sv
@@ -6,10 +6,10 @@ module mux4 (   input  logic        d0, d1, d2, d3,
 
     flipper i1 (S[0], Sbar0);
     flipper i2 (S[1], Sbar1);
-    and3 a1 (w1, d0, Sbar0, Sbar1);
-    and3 a2 (w2, d1, S[0], Sbar1);
-    and3 a3 (w3, d2, Sbar0, S[1]);
-    and3 a4 (w4, d3, S[0], S[1]);
+    and3 a1 (d0, Sbar0, Sbar1, w1);
+    and3 a2 (d1, S[0], Sbar1, w2);
+    and3 a3 (d2, Sbar0, S[1], w3);
+    and3 a4 (d3, S[0], S[1], w4);
     or4 o1 (Y, w1, w2, w3, w4);
 
 endmodule // mux41
