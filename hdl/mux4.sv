5e5033ba3376edc048b569a23cd5a0dcb2bfb707
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-04-29 13:09:49 -0500

    Fixed or gate input order

--- hdl/mux4.sv
+++ hdl/mux4.sv
@@ -10,6 +10,6 @@ module mux4 (   input  logic        d0, d1, d2, d3,
     and3 a2 (d1, S[0], Sbar1, w2);
     and3 a3 (d2, Sbar0, S[1], w3);
     and3 a4 (d3, S[0], S[1], w4);
-    or4 o1 (Y, w1, w2, w3, w4);
+    or4 o1 (w1, w2, w3, w4, Y);
 
 endmodule // mux41
