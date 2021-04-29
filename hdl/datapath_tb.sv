460db7fde603f66ef867e95e9399b6249aa842ed
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-04-28 21:42:38 -0500

    Changed timing in test bench so that registers enable early

--- hdl/datapath_tb.sv
+++ hdl/datapath_tb.sv
@@ -28,7 +28,7 @@ module stimulus;
    always
      begin
 	   desc3 = handle3;
-	   #5 $fdisplay(desc3, "%b %b %h", sel_ND_mux,
+	   #5 $fdisplay(desc3, "%b %b %b", sel_ND_mux,
 		     sel_K_mux, result);
      end
 
@@ -36,90 +36,79 @@ module stimulus;
      begin
         #0  reset <= 1'b0;
 
-        //Numbers are: 1.0460665924473482 / 1.9831498889813877 = 0.5274773219409266
+        // Numbers are: 1.0460665924473482 / 1.9831498889813877 = 0.5274773219409266
+        // Result should be near 1.0111100111110101
         #0  IA = 16'b01000000000000000;
     	#0  N = 16'b10000101111001011;
     	#0  D = 16'b11111101110101111;
 
     	//Cycle 1: IA * D
-    	#15  load_regN <= 1'b0;
+    	#4  load_regN <= 1'b0;
     	#0  load_regD <= 1'b1;
-    	#0  sel_ND_mux <= 2'b00;
+    	#1  sel_ND_mux <= 2'b00;
     	#0  sel_K_mux <= 1'b1;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b1;
 
     	//Cycle 2: IA * N
-    	#15  load_regN <= 1'b1;
+    	#14  load_regN <= 1'b1;
     	#0  load_regD <= 1'b0;
-    	#0  sel_ND_mux <= 2'b01;
+    	#1  sel_ND_mux <= 2'b01;
     	#0  sel_K_mux <= 1'b1;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
+
 
     	//Cycle 3: K1 * D
-    	#15  load_regN <= 1'b0;
+    	#14  load_regN <= 1'b0;
     	#0  load_regD <= 1'b1;
-    	#0  sel_ND_mux <= 2'b10;
+    	#1  sel_ND_mux <= 2'b10;
     	#0  sel_K_mux <= 1'b0;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
 
     	//Cycle 4: K1 * N
-    	#15  load_regN <= 1'b1;
+    	#14  load_regN <= 1'b1;
     	#0  load_regD <= 1'b0;
-    	#0  sel_ND_mux <= 2'b11;
+    	#1  sel_ND_mux <= 2'b11;
     	#0  sel_K_mux <= 1'b0;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
+
 
     	//Cycle 5: K2 * D
-    	#15  load_regN <= 1'b0;
+    	#14  load_regN <= 1'b0;
     	#0  load_regD <= 1'b1;
-    	#0  sel_ND_mux <= 2'b10;
+    	#1  sel_ND_mux <= 2'b10;
     	#0  sel_K_mux <= 1'b0;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
+
 
     	//Cycle 6: K2 * N
-    	#15  load_regN <= 1'b1;
+    	#14  load_regN <= 1'b1;
     	#0  load_regD <= 1'b0;
-    	#0  sel_ND_mux <= 2'b11;
+    	#1  sel_ND_mux <= 2'b11;
     	#0  sel_K_mux <= 1'b0;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
+
 
     	//Cycle 7: K3 * D
-    	#15  load_regN <= 1'b0;
+    	#14  load_regN <= 1'b0;
     	#0  load_regD <= 1'b1;
-    	#0  sel_ND_mux <= 2'b10;
+    	#1  sel_ND_mux <= 2'b10;
     	#0  sel_K_mux <= 1'b0;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
+
 
     	//Cycle 8: K3 * N
-    	#15  load_regN <= 1'b1;
+    	#14  load_regN <= 1'b1;
     	#0  load_regD <= 1'b0;
-    	#0  sel_ND_mux <= 2'b11;
+    	#1  sel_ND_mux <= 2'b11;
     	#0  sel_K_mux <= 1'b0;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
+
 
     	//Cycle 9: K4 * D
-    	#15  load_regN <= 1'b0;
+    	#14  load_regN <= 1'b0;
     	#0  load_regD <= 1'b1;
-    	#0  sel_ND_mux <= 2'b10;
+    	#1  sel_ND_mux <= 2'b10;
     	#0  sel_K_mux <= 1'b0;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
+
 
     	//Cycle 10: K4 * N
-    	#15  load_regN <= 1'b1;
+    	#14  load_regN <= 1'b1;
     	#0  load_regD <= 1'b0;
-    	#0  sel_ND_mux <= 2'b11;
+    	#1  sel_ND_mux <= 2'b11;
     	#0  sel_K_mux <= 1'b0;
-    	#5  load_regN <= 1'b0;
-    	#0  load_regD <= 1'b0;
+
 
      end
 
